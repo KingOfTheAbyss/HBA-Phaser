3a24e19dd5344c70d6c2ffa9aa1c7ebec7c92398
KingOfTheAbyss <kaesn05@gmail.com>
2017-06-29 14:59:54 -1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,81 +1,132 @@
-function init(){
-	game.renderer.renderSession.roundPixels = true;
+function init() {
+
 }
 
-function preload(){
-	game.load.json('level:1', 'data/level01.json');
-	game.load.image('background', 'images/background.png');
-	game.load.image('ground', 'images/ground.png');
+function preload() {
+    game.load.image('background', 'images/background.png');
+    game.load.json('level:1', 'data/level01.json');
+    game.load.image('ground', 'images/ground.png');
     game.load.image('grass:8x1', 'images/grass_8x1.png');
     game.load.image('grass:6x1', 'images/grass_6x1.png');
     game.load.image('grass:4x1', 'images/grass_4x1.png');
     game.load.image('grass:2x1', 'images/grass_2x1.png');
     game.load.image('grass:1x1', 'images/grass_1x1.png');
     game.load.image('hero', 'images/hero_stopped.png');
+    game.load.audio('sfx:jump', 'audio/jump.wav');
+    game.load.audio('sfx:coin', 'audio/coin.wav');
+    game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
+    game.load.spritesheet('spider', 'images/spider.png', 42, 32);
+    game.load.image('invisible-wall', 'images/invisible_wall.png');
 };
 
-function create(){
-	game.add.image(0, 0, 'background');
-	loadLevel(this.game.cache.getJSON('level:1'));
-	 leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-	 rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-	 upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-	 upKey.onDown.add(function(){
-};
+function create() {
+    game.add.image(0, 0, 'background');
+    sfxJump = game.add.audio('sfx:jump');
+    sfxCoin = game.add.audio('sfx:coin');
+    loadLevel(this.game.cache.getJSON('level:1'));
+    leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    upKey.onDown.add(function() {
+        jump();
+    });
+}
 
-function update(){
-	handleCollisions();
-	handleInput();
-};
+function update() {
+    handleInput();
+    handleCollisions();
+}
 
-//Create a game state
-
-var game = new Phaser.Game(960, 600, Phaser.AUTO, 'game', {init: init, preload: preload, create: create, update: update});
 function loadLevel(data) {
-	platforms = game.add.group();
+    console.log(data);
+    platforms = game.add.group();
+    coins = game.add.group();
+    spiders = game.add.group();
     data.platforms.forEach(spawnPlatform, this);
-    spawnCharacters({hero: data.hero});
+    spawnCharacters({
+        hero: data.hero,
+        spiders: data.spiders
+    });
+    data.coins.forEach(spawnCoin, this);
     game.physics.arcade.gravity.y = 1200;
-    
-    
+    enemyWalls = game.add.group();
 };
 
 function spawnPlatform(platform) {
     game.add.sprite(platform.x, platform.y, platform.image);
-     var sprite = platforms.create(platform.x, platform.y, platform.image);
+    var sprite = platforms.create(platform.x, platform.y, platform.image);
     game.physics.enable(sprite);
     sprite.body.allowGravity = false;
     sprite.body.immovable = true;
-}
-function spawnCharacters (data) {
-	hero = game.add.sprite(data.hero.x, data.hero.y, 'hero');
-	hero.anchor.set(0.5, 0.5);
-	game.physics.enable(hero);
-	 hero.body.collideWorldBounds = true;
 };
-function move(direction){
-    hero.body.velocity.x = direction * 200;
-    if (hero.body.velocity.x < 0) {
-        hero.scale.x = -1;
-    }
-    else if (hero.body.velocity.x > 0) {
-    	hero.scale.x = 1;
-    }
+
+function spawnCharacters(data) {
+    hero = game.add.sprite(data.hero.x, data.hero.y, 'hero');
+    hero.anchor.set(0.5, 0.5);
+    game.physics.enable(hero);
+    hero.body.collideWorldBounds = true;
+    data.spiders.forEach(function(spider) {
+        var sprite = game.add.sprite(spider.x, spider.y, 'spider');
+        spiders.add(sprite);
+        sprite.anchor.set(0.5);
+        sprite.animations.add('crawl', [0, 1, 2], 8, true);
+        sprite.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12);
+        sprite.animations.play('crawl');
+        game.physics.enable(sprite);
+        sprite.body.collideWorldBounds = true;
+        sprite.body.velocity.x = 100;
+    });
 }
-function handleInput(){
-    if (leftKey.isDown) {
-        move(-1);
+    function move(direction) {
+        hero.body.velocity.x = direction * 200;
+        if (hero.body.velocity.x < 0) {
+            hero.scale.x = -1;
+        } else if (hero.body.velocity.x > 0) {
+            hero.scale.x = 1;
+        }
     }
-    else if (rightKey.isDown) {
-    	move(1);
+
+    function handleInput() {
+        if (leftKey.isDown) {
+            move(-1);
+        } else if (rightKey.isDown) {
+            move(1);
+        } else {
+            move(0);
+        }
     }
-     else {
-        move(0);
+
+    function handleCollisions() {
+        game.physics.arcade.collide(hero, platforms);
+        game.physics.arcade.overlap(hero, coins, onHeroVsCoin, null);
+        game.physics.arcade.collide(spiders, platforms);
+    };
+
+    function jump() {
+        var canJump = hero.body.touching.down;
+        if (canJump) {
+            hero.body.velocity.y = -600;
+            sfxJump.play();
+        }
+        return canJump;
     }
-};
-function handleCollisions(){
-   game.physics.arcade.collide(hero, platforms);
-};
-function jump(){
-    hero.body.velocity.y = -600;
-}
+
+    function spawnCoin(coin) {
+        var sprite = coins.create(coin.x, coin.y, 'coin');
+        sprite.anchor.set(0.5, 0.5);
+        sprite.animations.add('rotate', [0, 1, 2, 1], 6, true);
+        sprite.animations.play('rotate');
+        game.physics.enable(sprite);
+        sprite.body.allowGravity = false;
+    };
+
+    function onHeroVsCoin(hero, coin) {
+        sfxCoin.play();
+        coin.kill();
+    };
+    var game = new Phaser.Game(960, 600, Phaser.AUTO, 'game', {
+        init: init,
+        preload: preload,
+        create: create,
+        update: update
+    });
